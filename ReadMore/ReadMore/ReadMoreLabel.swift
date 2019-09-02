f09d9b923fafc8d2cdf3cde0fd3254bad32ee3b9
Joey Zhang <joey.zhang@tgitech.net>
2019-09-02 11:56:36 +0800

    feat: custom label

---
+++ ReadMore/ReadMore/ReadMoreLabel.swift
@@ -0,0 +1,119 @@
+//
+//  ReadMoreLabel.swift
+//  ReadMore
+//
+//  Created by joey on 9/2/19.
+//  Copyright © 2019 TGI Technology. All rights reserved.
+//
+
+import UIKit
+
+class ReadMoreLabel: UILabel {
+
+    fileprivate var _fullText: String?
+
+    var fullText: String? {
+        get {
+            return _fullText
+        }
+        set {
+            _fullText = newValue
+            text = newValue
+        }
+    }
+
+    var isFullTextShown: Bool = false {
+        didSet {
+            if isFullTextShown {
+                showFullText()
+            } else {
+                showSomeText()
+            }
+        }
+    }
+
+    func showSomeText() {
+        self.numberOfLines = 3
+        if let text = self.text, text.count > 1 {
+            let readmoreFont = UIFont.systemFont(ofSize: 12)
+            let readmoreFontColor = UIColor.blue
+            DispatchQueue.main.async {
+                self.addTrailing(with: "...", moreText: "Readmore", moreTextFont: readmoreFont, moreTextColor: readmoreFontColor)
+            }
+//            let readMoreGesture = UITapGestureRecognizer(target: self, action: #selector(labelAction))
+//            readMoreGesture.numberOfTapsRequired = 1
+//            self.addGestureRecognizer(readMoreGesture)
+        }
+    }
+
+    func showFullText() {
+        self.numberOfLines = 0
+        self.text = fullText
+        self.sizeToFit()
+    }
+
+//    @objc func labelAction() {
+//        isFullTextShown = isFullTextShown ? false : true
+//    }
+}
+
+fileprivate extension UILabel{
+
+    func addTrailing(with trailingText: String, moreText: String, moreTextFont: UIFont, moreTextColor: UIColor) {
+
+        let readMoreText: String = trailingText + moreText
+
+        if let myText = self.text, self.visibleTextLength > 0 {
+            let lengthForVisibleString: Int = self.visibleTextLength
+            let mutableString: String = myText
+            let trimmedString: String? = (mutableString as NSString).replacingCharacters(in: NSRange(location: lengthForVisibleString, length: myText.count - lengthForVisibleString), with: "")
+            let readMoreLength: Int = (readMoreText.count)
+
+            if let safeTrimmedString = trimmedString, safeTrimmedString.count > readMoreLength {
+                let trimmedForReadMore: String = (safeTrimmedString as NSString).replacingCharacters(in: NSRange(location: safeTrimmedString.count - readMoreLength, length: readMoreLength), with: "") + trailingText
+                let answerAttributed = NSMutableAttributedString(string: trimmedForReadMore, attributes: [NSAttributedString.Key.font: self.font])
+                let readMoreAttributed = NSMutableAttributedString(string: moreText, attributes: [NSAttributedString.Key.font: moreTextFont, NSAttributedString.Key.foregroundColor: moreTextColor])
+                answerAttributed.append(readMoreAttributed)
+                self.attributedText = answerAttributed
+            }
+        }
+    }
+
+    var visibleTextLength: Int {
+
+        let font: UIFont = self.font
+        let mode: NSLineBreakMode = self.lineBreakMode
+        let labelWidth: CGFloat = self.frame.size.width
+        let labelHeight: CGFloat = self.frame.size.height
+        let sizeConstraint = CGSize(width: labelWidth, height: CGFloat.greatestFiniteMagnitude)
+
+        if let myText = self.text {
+
+            let attributes: [AnyHashable: Any] = [NSAttributedString.Key.font: font]
+            let attributedText = NSAttributedString(string: myText, attributes: attributes as? [NSAttributedString.Key : Any])
+            let boundingRect: CGRect = attributedText.boundingRect(with: sizeConstraint, options: .usesLineFragmentOrigin, context: nil)
+
+            if boundingRect.size.height > labelHeight {
+                var index: Int = 0
+                var prev: Int = 0
+                let characterSet = CharacterSet.whitespacesAndNewlines
+                repeat {
+                    prev = index
+                    if mode == NSLineBreakMode.byCharWrapping {
+                        index += 1
+                    } else {
+                        index = (myText as NSString).rangeOfCharacter(from: characterSet, options: [], range: NSRange(location: index + 1, length: myText.count - index - 1)).location
+                    }
+                } while index != NSNotFound && index < myText.count && (myText as NSString).substring(to: index).boundingRect(with: sizeConstraint, options: .usesLineFragmentOrigin, attributes: attributes as? [NSAttributedString.Key : Any], context: nil).size.height <= labelHeight
+                return prev
+            }
+        }
+
+        if self.text == nil {
+            return 0
+        } else {
+            return self.text!.count
+        }
+    }
+}
+
--- ReadMore/ReadMore/ViewController.swift
+++ ReadMore/ReadMore/ViewController.swift
@@ -10,111 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var someLabel: UILabel! {
-        didSet {
-            someLabel.text = fullText
-        }
-    }
+    @IBOutlet weak var someLabel: ReadMoreLabel!
 
     fileprivate let fullText = "This style guide is based on Apple’s excellent Swift standard library style and also incorporates feedback from usage across multiple Swift projects within Google. It is a living document and the basis upon which the formatter is implemented."
 
-    fileprivate var isFullTextShown: Bool = false {
-        didSet {
-            if isFullTextShown {
-                showFullText()
-            } else {
-                showSomeText()
-            }
-        }
-    }
-
     override func viewDidLoad() {
-        super.viewDidLoad()
-        showSomeText()
+        someLabel.fullText = fullText
+        someLabel.isFullTextShown = false
     }
 
-    fileprivate func showSomeText() {
-        someLabel.numberOfLines = 3
-        if let text = someLabel.text, text.count > 1 {
-            let readmoreFont = UIFont.systemFont(ofSize: 12)
-            let readmoreFontColor = UIColor.blue
-            DispatchQueue.main.async {
-                self.someLabel.addTrailing(with: "...", moreText: "Readmore", moreTextFont: readmoreFont, moreTextColor: readmoreFontColor)
-            }
-            let readMoreGesture = UITapGestureRecognizer(target: self, action: #selector(labelAction))
-            readMoreGesture.numberOfTapsRequired = 1
-            someLabel.addGestureRecognizer(readMoreGesture)
-        }
-    }
-
-    fileprivate func showFullText() {
-        self.someLabel.numberOfLines = 0
-        self.someLabel.text = fullText
-        self.someLabel.sizeToFit()
-    }
-
-    @objc func labelAction() {
-        isFullTextShown = isFullTextShown ? false : true
-    }
-
-}
-
-extension UILabel{
-
-    func addTrailing(with trailingText: String, moreText: String, moreTextFont: UIFont, moreTextColor: UIColor) {
-
-        let readMoreText: String = trailingText + moreText
-
-        if let myText = self.text, self.visibleTextLength > 0 {
-            let lengthForVisibleString: Int = self.visibleTextLength
-            let mutableString: String = myText
-            let trimmedString: String? = (mutableString as NSString).replacingCharacters(in: NSRange(location: lengthForVisibleString, length: myText.count - lengthForVisibleString), with: "")
-            let readMoreLength: Int = (readMoreText.count)
-
-            if let safeTrimmedString = trimmedString, safeTrimmedString.count > readMoreLength {
-                let trimmedForReadMore: String = (safeTrimmedString as NSString).replacingCharacters(in: NSRange(location: safeTrimmedString.count - readMoreLength, length: readMoreLength), with: "") + trailingText
-                let answerAttributed = NSMutableAttributedString(string: trimmedForReadMore, attributes: [NSAttributedString.Key.font: self.font])
-                let readMoreAttributed = NSMutableAttributedString(string: moreText, attributes: [NSAttributedString.Key.font: moreTextFont, NSAttributedString.Key.foregroundColor: moreTextColor])
-                answerAttributed.append(readMoreAttributed)
-                self.attributedText = answerAttributed
-            }
-        }
-    }
-
-    var visibleTextLength: Int {
-
-        let font: UIFont = self.font
-        let mode: NSLineBreakMode = self.lineBreakMode
-        let labelWidth: CGFloat = self.frame.size.width
-        let labelHeight: CGFloat = self.frame.size.height
-        let sizeConstraint = CGSize(width: labelWidth, height: CGFloat.greatestFiniteMagnitude)
-
-        if let myText = self.text {
-
-            let attributes: [AnyHashable: Any] = [NSAttributedString.Key.font: font]
-            let attributedText = NSAttributedString(string: myText, attributes: attributes as? [NSAttributedString.Key : Any])
-            let boundingRect: CGRect = attributedText.boundingRect(with: sizeConstraint, options: .usesLineFragmentOrigin, context: nil)
-
-            if boundingRect.size.height > labelHeight {
-                var index: Int = 0
-                var prev: Int = 0
-                let characterSet = CharacterSet.whitespacesAndNewlines
-                repeat {
-                    prev = index
-                    if mode == NSLineBreakMode.byCharWrapping {
-                        index += 1
-                    } else {
-                        index = (myText as NSString).rangeOfCharacter(from: characterSet, options: [], range: NSRange(location: index + 1, length: myText.count - index - 1)).location
-                    }
-                } while index != NSNotFound && index < myText.count && (myText as NSString).substring(to: index).boundingRect(with: sizeConstraint, options: .usesLineFragmentOrigin, attributes: attributes as? [NSAttributedString.Key : Any], context: nil).size.height <= labelHeight
-                return prev
-            }
-        }
-
-        if self.text == nil {
-            return 0
-        } else {
-            return self.text!.count
-        }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        someLabel.isFullTextShown = someLabel.isFullTextShown ? false : true
     }
 }
